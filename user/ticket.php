28c634bf56041b96cb8a122eae4644fea7f22a87
hirotowatanabe <hiroto.private@gmail.com>
2018-01-11 02:35:28 +0900

    session

--- user/ticket.php
+++ user/ticket.php
@@ -3,22 +3,26 @@ header("Content-Type:text/html; charset=UTF-8");
 include($_SERVER['DOCUMENT_ROOT']."/login_chk.php");
 $pageTitle = "選択中のチケット";
 if(isset($_POST["menuSelectSubmit"])){
-    $_SESSION["ticket"] = array(
-        'id' => $_POST["id"],
-        'num' => $_POST["num"]
+    $menuArray = array(
+        $_POST["id"] => $_POST["num"]
     );
+    $_SESSION["ticket"][] = $menuArray;
     header("Location: /");
     exit();
 }
 
-/*
-if(isset($_SESSION)){
+if(isset($_GET["reset"])){
+    session_unset($_SESSION["ticket"]);
+}
+if(isset($_SESSION["ticket"])){
+    print_r($_SESSION["ticket"]);
+    /*
     foreach($_SESSION as $name => $value){
         $id[] = $name;
         $num[] = $value;
     }
+    */
 }
-*/
 ?>
 <!DOCTYPE html>
 <?php include($_SERVER['DOCUMENT_ROOT']."/head.php") ?>
@@ -26,9 +30,10 @@ if(isset($_SESSION)){
     <?php include($_SERVER['DOCUMENT_ROOT']."/header.php") ?>
     <main class="user-main">
         <?php if(isset($_SESSION["ticket"])): ?>
-            <?php for($i = 0;$i < count($id);$i++){ ?>
+            <?php //for($i = 0;$i < count($id);$i++){ ?>
             <!--display ticket-->
-            <?php } ?>
+            <?php //} ?>
+            <a href="ticket.php?reset=true">選択中のチケットをリセット</a>
         <?php else: ?>
             選択中のチケットはありません。
         <?php endif; ?>

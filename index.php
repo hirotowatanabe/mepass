e20a8291f0ea56e739d87be87d451f142d90dbdb
hirotowatanabe <hiroto.private@gmail.com>
2018-01-13 23:48:21 +0900

    support pdo etc

--- index.php
+++ index.php
@@ -1,44 +1,31 @@
 <?php
-header("Content-Type:text/html; charset=UTF-8");
-include("login_chk.php");
-$pageTitle = "フィード";
-$searchValue = "";
+header('Content-Type:text/html; charset=UTF-8');
+include('login_chk.php');
+$pageTitle = 'フィード';
+$searchValue = '';
 
-if(isset($_POST["searchButton"])){
-    $searchValue = $_POST["searchValue"];
+if(isset($_POST['searchButton'])){
+    $searchValue = $_POST['searchValue'];
 }
 
-include($_SERVER['DOCUMENT_ROOT']."/mysqlenv.php");
-
-if(!$Link = mysqli_connect($HOST,$USER,$PASS)){
-    exit("MySQL接続エラー<br />" . mysqli_connect_error());
+include($_SERVER['DOCUMENT_ROOT'].'/mysqlenv.php');
+try{
+    $dbh = new PDO($pdoDsn, $pdoUser, $pdoPass);
+    if($dbh == null){
+        exit('DB接続失敗');
+    }
+    $dbh->query('set names utf8');
+    $sql = "select * from t_menu where menu_name like '%".$searchValue."%'";
+    $stmt = $dbh->query($sql);
+    while($result = $stmt->fetch(PDO::FETCH_ASSOC)){
+        $rows[] = $result;
+    }
+    $count = $stmt->rowCount();
+}catch(PDOException $e){
+    echo 'Error:'.$e->getMessage();
+    die();
 }
-
-$SQL = "set names utf8";
-if(!mysqli_query($Link,$SQL)){
-    exit("MySQLクエリー送信エラー<br />" . $SQL);
-}
-
-if(!mysqli_select_db($Link,$DB)){
-    exit("MySQLデータベース選択エラー<br />" . $DB);
-}
-
-$SQL = "select * from t_menu where menu_name like '%".$searchValue."%'";
-if(!$SqlRes = mysqli_query($Link,$SQL)){
-    exit("MySQLクエリー送信エラー<br />" . mysqli_error($Link) . "<br />" . $SQL);
-}
-
-while($Row = mysqli_fetch_array($SqlRes)){
-    $RowAry[] = $Row;
-}
-
-$NumRow = mysqli_num_rows($SqlRes);
-
-mysqli_free_result($SqlRes);
-
-if(!mysqli_close($Link)){
-    exit("MySQL切断エラー");
-}
+$dbh = null;
 ?>
 <!DOCTYPE html>
 <?php include($_SERVER['DOCUMENT_ROOT']."/head.php") ?>
@@ -46,18 +33,18 @@ if(!mysqli_close($Link)){
     <?php include($_SERVER['DOCUMENT_ROOT']."/header.php") ?>
     <main class="user-main">
         <form class="user-main__search" action="index.php" method="post">
-            <input class="admin-main-top-form__text" type="text" name="searchValue" value="<?php print $searchValue; ?>" placeholder="メニュー名検索" />
+            <input class="admin-main-top-form__text" type="text" name="searchValue" value="<?= $searchValue ?>" placeholder="メニュー名検索" />
             <input class="admin-main-top-form__submit" type="submit" name="searchButton" value="検索" />
         </form>
         <div class="admin-main__menu">
-        <?php if($NumRow != 0): ?>
-            <?php for($i=0; $i<$NumRow; $i++): ?>
+        <?php if($count != 0): ?>
+            <?php for($i=0; $i<$count; $i++): ?>
                 <div class="menu-card">
-                    <img src="/store/menu/images/<?php print $RowAry[$i]["menu_file_name"]; ?>" width="300">
-                    <div class="menu-card__name"><?php print $RowAry[$i]["menu_name"]; ?></div>
-                    <div class="menu-card__price"><?php print $RowAry[$i]["menu_price"]; ?>円</div>
+                    <img src="/store/menu/images/<?= $rows[$i]['menu_file_name'] ?>" width="300">
+                    <div class="menu-card__name"><?= $rows[$i]['menu_name'] ?></div>
+                    <div class="menu-card__price"><?= $rows[$i]['menu_price'] ?>円</div>
                     <form action="/user/ticket.php" method="post">
-                        <input type="hidden" name="id" value="<?php print $RowAry[$i]["menu_num"]; ?>">
+                        <input type="hidden" name="id" value="<?= $result[$i]['menu_num'] ?>">
                         <input class="menu-card-form__number" type="number" name="num" value="1">点
                         <input class="menu-card-form__submit" type="submit" name="menuSelectSubmit" value="選択">
                     </form>

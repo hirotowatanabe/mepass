fad4b69ba8c259b950f8e56a342d55ff255996df
hirotowatanabe <hiroto.private@gmail.com>
2018-01-16 01:39:58 +0900

    add mypage

---
+++ user/account/mypage/index.php
@@ -0,0 +1,35 @@
+<?php
+header('Content-Type:text/html; charset=UTF-8');
+include($_SERVER['DOCUMENT_ROOT'].'/login_chk.php');
+$pageTitle = 'マイページ';
+/*
+include($_SERVER['DOCUMENT_ROOT'].'/mysqlenv.php');
+try{
+    $dbh = new PDO($pdoDsn, $pdoUser, $pdoPass);
+    if($dbh == null){
+        exit('DB接続失敗');
+    }
+    $dbh->query('set names utf8');
+    $sql = "select * from t_order where mem_mail = '".$UserMail."'";
+    $stmt = $dbh->query($sql);
+    while($result = $stmt->fetch(PDO::FETCH_ASSOC)){
+        $rows[] = $result;
+    }
+    $count = $stmt->rowCount();
+}catch(PDOException $e){
+    echo 'Error:'.$e->getMessage();
+    die();
+}
+$dbh = null;
+*/
+?>
+<!DOCTYPE html>
+<?php include($_SERVER['DOCUMENT_ROOT']."/head.php") ?>
+<body>
+    <?php include($_SERVER['DOCUMENT_ROOT']."/header.php") ?>
+    <main class="user-main">
+        <a class="user-main-ticket-top__reset" href="/user/delete/">退会</a>
+    </main>
+    <?php include($_SERVER['DOCUMENT_ROOT']."/footer.php") ?>
+</body>
+</html>

dd0a9c7aea2d9852b48636ce875d4ac49f82d887
hirotowatanabe <hiroto.private@gmail.com>
2018-01-16 14:04:07 +0900

    fix logout

--- company/account/logout/index.php
+++ company/account/logout/index.php
@@ -1,9 +1,7 @@
 <?php
+header('Content-Type:text/html; charset=UTF-8');
 session_start();
-session_unset($_SESSION["company"]);
-header("Location: /company/account/login/");
+unset($_SESSION['company']);
+header('Location: /company/account/login/');
 exit();
-
-//  HTTPヘッダーで文字コードを指定
-//header("Content-Type:text/html; charset=UTF-8");
 ?>
